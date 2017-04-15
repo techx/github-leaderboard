8b290413f463f6fd234f3975101b22b47f4f9357
Shreyas Kapur <shreyask@mit.edu>
2017-04-15 19:46:14 -0400

    spaces win rip

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from stats import *
 app = Flask(__name__)
 
 def GithubPoller():
-	pass
+    pass
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+    return "Hello World!"
 
 if __name__ == "__main__":
-	scheduler = greenclock.Scheduler(logger_name='task_scheduler')
-	scheduler.schedule('GithubPollTask', greenclock.every_second(GITHUB_POLL_TIMER), GithubPoller)
-	app.run(host="0.0.0.0", port=80, debug=True)
+    scheduler = greenclock.Scheduler(logger_name='task_scheduler')
+    scheduler.schedule('GithubPollTask', greenclock.every_second(GITHUB_POLL_TIMER), GithubPoller)
+    app.run(host="0.0.0.0", port=80, debug=True)
--- stats.py
+++ stats.py
@@ -5,25 +5,25 @@ g = Github(username, password)
 org = g.get_organization("techx")
 
 def get_commits(num_weeks):
-	contribs = {}
+    contribs = {}
 
-	for repo in org.get_repos():
-		conts = repo.get_stats_contributors()
-		if conts is None:
-			continue
-		for c in conts:
-			aggr = 0
-			for w, i in zip(c.weeks, range(len(c.weeks))):
-				if i >= num_weeks:
-					break
-				aggr += w.c # Count commits
-			if c.author.name in contribs:
-				contribs[c.author.name] += aggr
-			else:
-				contribs[c.author.name] = aggr
-		print ""
+    for repo in org.get_repos():
+        conts = repo.get_stats_contributors()
+        if conts is None:
+            continue
+        for c in conts:
+            aggr = 0
+            for w, i in zip(c.weeks, range(len(c.weeks))):
+                if i >= num_weeks:
+                    break
+                aggr += w.c # Count commits
+            if c.author.name in contribs:
+                contribs[c.author.name] += aggr
+            else:
+                contribs[c.author.name] = aggr
+        print ""
 
-	ordered = []
-	for w in sorted(contribs, key=contribs.get, reverse=True):
-		ordered.append({"name": w, "commits": contribs[w]})
-	return ordered
+    ordered = []
+    for w in sorted(contribs, key=contribs.get, reverse=True):
+        ordered.append({"name": w, "commits": contribs[w]})
+    return ordered

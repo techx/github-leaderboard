5eef755a0c6200f5ae00bb6ad3c602ff63c4a043
Anthony Liu <anthonyscottliu@gmail.com>
2017-04-16 01:38:40 -0400

    Loads the leaderboard data from the api endpoint.

--- static/js/dash.js
+++ static/js/dash.js
@@ -1,6 +1,7 @@
 var Dash = (function() {
   // config
   var DATE_UPDATE_FREQUENCY = 5000;
+  var COMMIT_UPDATE_FREQUENCY = 10*1000;
 
   // constants
   var MONTH_NAMES = [
@@ -38,13 +39,21 @@ var Dash = (function() {
   }
 
   function initCommits() {
-    populateLeaderboard([
-      {username: 'turbomaze', commits: 14},
-      {username: 'revalo', commits: 12},
-      {username: 'jjz', commits: 10},
-      {username: 'vahidfazelrezai', commits: 4},
-      {username: 'moinnadeem', commits: 4}
-    ]);
+    updateLeaderboard();
+    // setInterval(updateLeaderboard, COMMIT_UPDATE_FREQUENCY);
+  }
+
+  function updateLeaderboard() {
+    $.get(
+      '/api/panel'
+    ).done(function(raw) {
+      var data = JSON.parse(raw);
+      if ('leaderboard' in data) {
+        populateLeaderboard(data.leaderboard);
+      } else {
+        // uh-oh
+      }
+    });
   }
 
 
@@ -59,7 +68,7 @@ var Dash = (function() {
       var li = document.createElement('li'); 
       var username = document.createElement('span'); 
       username.className = 'username';
-      username.innerHTML = leader.username;
+      username.innerHTML = leader.name;
       var commits = document.createElement('span'); 
       commits.className = 'commits';
       commits.innerHTML = leader.commits;
